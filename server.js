ca0f04b5fefd5b55301c26da7a340e90d70f7a8e
David Byman <dbvt@databass.com>
2014-03-10 13:31:04 -0400

    Update server.js
    misc console cleanup, formatting

--- server.js
+++ server.js
@@ -1,35 +1,39 @@
-var express = require('express'), app = express(), request = require('request'), path = require('path'), repoman = require('./repoman.js'), port = process.argv[2] || 3000;
+var express = require('express'),
+    app = express(),
+    request = require('request'),
+    path = require('path'),
+    repoman = require('./repoman.js'),
+    port = process.argv[2] || 3000;
 
 app.use(express.static(path.join(__dirname + "/public", '')));
 
 // get org info for a github organization.
-app.get('/org/:orgname', function(req, res) {
+app.get('/org/:orgname', function (req, res) {
 
     var orgName = req.params.orgname;
     res.setHeader('content-type', 'application/json');
-    return repoman.getOrgInfo(orgName, function(orgInfo) {
+    return repoman.getOrgInfo(orgName, function (orgInfo) {
         res.send(orgInfo);
     });
 });
 
 // get repo info for a github organization.
-app.get('/repos/:orgname', function(req, res) {
+app.get('/repos/:orgname', function (req, res) {
 
     var orgName = req.params.orgname;
     res.setHeader('content-type', 'application/json');
-    return repoman.getRepos(orgName, function(repos) {
-        console.log("repos is " + JSON.stringify(repos));
+    return repoman.getRepos(orgName, function (repos) {
         res.send(repos);
         res.end();
     });
 });
 
 // get commit info for a github organization.
-app.get('/commits/:orgname/:reponame', function(req, res) {
-    var orgName = req.params.orgname, repoName = req.params.reponame;
+app.get('/commits/:orgname/:reponame', function (req, res) {
+    var orgName = req.params.orgname,
+        repoName = req.params.reponame;
     res.setHeader('content-type', 'application/json');
-    return repoman.getCommits(orgName, repoName, function(commits) {
-        console.log(repoName + " commits  " + JSON.stringify(commits));
+    return repoman.getCommits(orgName, repoName, function (commits) {
         res.send(commits);
     });
 });
