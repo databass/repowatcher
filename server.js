7d6fe83ae60e222b16b4306cae2aaef4a8845af7
David Byman <dbvt@databass.com>
2014-03-22 04:47:17 -0400

    Update server.js
    added multiple owner types, not just orgs

--- server.js
+++ server.js
@@ -1,41 +1,46 @@
-var express = require('express'),
-    app = express(),
-    request = require('request'),
-    path = require('path'),
-    repoman = require('./repoman.js'),
-    port = process.argv[2] || 3000;
+var express = require('express'), app = express(), request = require('request'), path = require('path'), repoman = require('./repoman.js'), port = process.argv[2] || 3000;
 
 app.use(express.static(path.join(__dirname + "/public", '')));
 
 // get org info for a github organization.
-app.get('/org/:orgname', function (req, res) {
+app.get('/org/:orgname', function(req, res) {
 
     var orgName = req.params.orgname;
     res.setHeader('content-type', 'application/json');
-    return repoman.getOrgInfo(orgName, function (orgInfo) {
-        res.send(orgInfo);
+    return repoman.getOrgInfo(orgName, function(orgInfo) {
+	res.send(orgInfo);
     });
 });
 
 // get repo info for a github organization.
-app.get('/repos/:orgname', function (req, res) {
+app.get('/repos/:ownertype/:orgname', function(req, res) {
 
-    var orgName = req.params.orgname;
+    var orgName = req.params.orgname, ownerType = req.params.ownertype;
+
+
     res.setHeader('content-type', 'application/json');
-    return repoman.getRepos(orgName, function (repos) {
-        res.send(repos);
-        res.end();
+    return repoman.getRepos(orgName, ownerType, function(repos) {
+	console.log("repos is " + JSON.stringify(repos));
+	res.send(repos);
+	res.end();
     });
 });
 
 // get commit info for a github organization.
-app.get('/commits/:orgname/:reponame', function (req, res) {
-    var orgName = req.params.orgname,
-        repoName = req.params.reponame;
+app.get('/commits/:orgname/:reponame', function(req, res) {
+    // https://api.github.com/repos/netflix/rxjava/commits
+    var orgName = req.params.orgname, repoName = req.params.reponame;
     res.setHeader('content-type', 'application/json');
-    return repoman.getCommits(orgName, repoName, function (commits) {
-        res.send(commits);
+    return repoman.getCommits(orgName, repoName, function(commits) {
+	console.log(repoName + " commits  " + JSON.stringify(commits));
+	res.send(commits);
     });
 });
+
+// load an owner's repositories
+app.get('/:orgname', function(req, res) {
+	app.use(express.static(path.join(__dirname + "/public/index.html?" + req.params.orgname, '')));
+});
+
 
 app.listen(port);
